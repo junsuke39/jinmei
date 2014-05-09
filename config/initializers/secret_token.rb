4bfbb6fb4a6bcd98ba4d0b6cf546b1d6b8a851df
junsuke <junsuke39@gmail.com>
2014-05-09 15:14:33 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jinmei::Application.config.secret_key_base = '01276eca4e3e26bbc16a3677b9d75c31cb22ecfef8b51727aeb027dc635e52db1398260c54a6d209addc9e40f3f1c1c2a5de93a956cdbecaf0840e9c0cde1d2a'
